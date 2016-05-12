07755fe701a5dbea92f762e5c46c8ac5571417d9
gk61gre <jmgsquared214@gmail.com>
2016-05-11 21:03:20 -0700

    adding YTsearch and searchbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from 'search_bar';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyANVl3QA0_NWKwmWPpUVAls4clg_zkX7mY';
 
+YTSearch({key: API_KEY, term: 'kittens'}, function(data) {
+    console.log(data);
+
+});
+
 const App = () => {
-    return <div>VideoPage</div>;
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
