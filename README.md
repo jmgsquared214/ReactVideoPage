fa7d84142760eba135a81dba46cbf0022e586bc3
wendyhuanglj <lijuan.huang@csaa.com>
2016-06-02 17:54:51 -0700

    added style and css

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ git clone git@github.com:jmgsquared214/ReactVideoPage.git
    > cd ReactVideoPage
    > npm install
    > npm start
+
+
+class based component vs. functional component
+
+state = whenever change our state, the component instantly re-renders along with any children.
+
+callbacks for parent child communication = react only application (2 times) , in redux fewer call backs
+
+
+redux.js.org
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 
+
+//use class whenever use concept of state
 class SearchBar extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { term: ''};
+        this.state = { term: '' };
     }
 
     render() {
     return (
-        <div>
+        <div className="search-bar">
           <input
               value={this.state.term}
-              onChange={event => this.setState({ term: event.target.value })} />
+              onChange={event => this.onInputChange(event.target.value)} />
         </div>
        );
     }
+
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
 }
 export default SearchBar;
